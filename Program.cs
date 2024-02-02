8ad2db234bac1aeae78a4d0d9351c81715fa769f
Fabricio Silva da Rocha <fsilvarocha@outlook.com>
2024-02-02 10:21:12 -0300

    Configurações iniciais

--- Program.cs
+++ Program.cs
@@ -1,7 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using WebIdentity.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+var connString = builder.Configuration.GetConnectionString("SqlServerConnection");
+
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connString));
 
 var app = builder.Build();
 
